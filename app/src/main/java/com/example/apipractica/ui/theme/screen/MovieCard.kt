ace472bafc43fbe1d0c529c015d2afe126565050
Andre <galvezcastroc@gmail.com>
2025-03-05 20:43:58 +0100

    Modifica un par de cosas :p

---
+++ app/src/main/java/com/example/apipractica/ui/theme/screen/MovieCard.kt
@@ -0,0 +1,45 @@
+package com.example.apipractica.ui.theme.screen
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.apipractica.data.modelo.Movie
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.layout.ContentScale
+import coil.compose.rememberImagePainter
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun MovieCard(movie: Movie, navController: NavController) { // <-- Agregado navController
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .clickable {
+                navController.navigate("movie_detail/${movie.id}") // <-- Navegar al detalle
+            },
+        elevation = CardDefaults.cardElevation(4.dp)
+    ) {
+        Column (modifier = Modifier.padding(16.dp)) {
+            Image(
+                painter = rememberImagePainter("https://image.tmdb.org/t/p/w500${movie.posterPath}"),
+                contentDescription = movie.title,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.height(200.dp).fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = movie.title, style = MaterialTheme.typography.titleMedium)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = movie.overview, style = MaterialTheme.typography.bodySmall)
+        }
+    }
+}
